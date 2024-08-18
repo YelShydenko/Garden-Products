3b18bddfb055a12f8c74284352c5d98d2eb882ac
Yelyzaveta Shydenko <lizawinsdale@gmail.com>
2024-08-18 16:12:51 +0200

    fixed productCard and categoriesProduct

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,38 +1,33 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../../ThemeContext/ThemeContext";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 import "./ProductCard.scss";
 
-
 const ProductCard = ({ product }) => {
-const { theme } = useContext(ThemeContext);
-
+  const { theme } = useContext(ThemeContext);
 
   const discountPercentage = product.discont_price
-  ? Math.round(
-    // Вычисляем размер скидки
-    ((product.price - product.discont_price) / product.price) * 100)
-  : 0
+    ? Math.round(
+        // Вычисляем размер скидки
+        ((product.price - product.discont_price) / product.price) * 100
+      )
+    : 0;
 
-  
   return (
-    <li className={`product__card card-${theme}`}>
+    <div className={`product__card card-${theme}`}>
       <div className="product__image-container">
         <div className="img_buttonWrapper">
-             <img
-          src={`https://exam-server-5c4e.onrender.com${product.image}`}
-          alt={product.title}
-          width={316}
-          height={284}
-          className={`product__image image-${theme}`}
-        />0
+          <img
+            src={`https://exam-server-5c4e.onrender.com${product.image}`}
+            alt={product.title}
+            className={`product__image image-${theme}`}
+          />
         </div>
-        
         <div className="product__image-icons">
-          <IoMdHeartEmpty className="icon"/>
-          <HiOutlineShoppingBag className="icon"/>
+          <IoMdHeartEmpty className="icon" />
+          <HiOutlineShoppingBag className="icon" />
         </div>
         {product.discont_price && product.discont_price > 0 && (
           <span className="discount__badge">{`-${discountPercentage}%`}</span>
@@ -42,29 +37,24 @@ const { theme } = useContext(ThemeContext);
         <div className="product__info">
           <h3 className={`product__name name-${theme}`}>{product.title}</h3>
           <div className="product__pricing">
-            {
-              
-              product.discont_price !== null  && product.discont_price > 0 && (<>
-             
+            {product.discont_price !== null && product.discont_price > 0 ? (
+              <>
                 <span className={`product__price price-${theme}`}>
-              ${product.discont_price?.toFixed(2)}
+                  ${product.discont_price.toFixed(2)}
                 </span>
-
-              <span className="original__price">
-              ${product.price.toFixed(2)}
-              </span>
-              </>  
-              )
-              }
-            {product.discont_price === null && (
+                <span className="original__price">
+                  ${product.price.toFixed(2)}
+                </span>
+              </>
+            ) : (
               <span className={`product__price price-${theme}`}>
                 ${product.price.toFixed(2)}
               </span>
-            )} 
+            )}
           </div>
         </div>
       </Link>
-    </li>
+    </div>
   );
 };
 
