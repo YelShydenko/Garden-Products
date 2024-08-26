415589bb59d99142a1873caac2b621c643f57499
Yelyzaveta Shydenko <lizawinsdale@gmail.com>
2024-08-26 11:43:14 +0200

    fixed error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
+import { useContext } from "react";
 import "./ErrorPage.scss";
-import imgage1 from "../../../public/images/4.png";
-import imgage2 from "../../../public/images/cactusError.png";
-import imgage3 from "../../../public/images/5.png";  
-import Button from '../../Components/UI/Button/Button'; 
+import cactusError from "/images/cactusError.png";
+import errorNumber from "/images/errorNumber.png";
+import Button from "@/Components/UI/Button/Button";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "@/ThemeContext/ThemeContext";
 
 const ErrorPage = () => {
+  const { theme } = useContext(ThemeContext);
   return (
     <>
       <div className="not-found__content">
         <div className="content">
           <div className="content__item">
             <div className="content__item__number">
-              <img src={imgage1} alt="4" />
+              <img src={errorNumber} alt="4" />
             </div>
             <div className="content__item__image">
-              <img src={imgage2} alt="Cactus" />
+              <img src={cactusError} alt="Cactus" />
             </div>
             <div className="content__item__number">
-              <img src={imgage3} alt="4" />
+              <img src={errorNumber} alt="4" />
             </div>
           </div>
           <div className="content__text">
-            <h2>Page Not Found</h2>
-            <span>We're sorry, the page you requested could not be found.</span>
-            <p> Please go back to the homepage.</p>
+            <h2 className={`error-${theme}`}>
+              Page Not Found
+            </h2>
+            <span className={`error-${theme}`}>
+              We're sorry, the page you requested could not be found.
+            </span>
+            <p className={`error-${theme}`}>
+              Please go back to the homepage.
+            </p>
             <Link to={"/"}>
-            <Button
-              btnColor={"green"}
-              btnSize={"M"}
-              btnText={"Go Home"}
-            />
-             </Link>
+              <Button btnColor={"green"} btnSize={"M"} btnText={"Go Home"} />
+            </Link>
           </div>
         </div>
       </div>
